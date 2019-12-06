d2ac0aac55c1b794a15c8a438b8e945880b09d3c
Michał Balicki <michalbalicki@o2.pl>
2019-12-06 22:20:16 +0100

    Draw grid refactoring and small adjustments in Game.cpp

--- ChessDB/Game.cpp
+++ ChessDB/Game.cpp
@@ -6,7 +6,7 @@ void Game::LoadConfigFile()
 	std::string line;
 	fvar.open("settings\\general.txt", std::ios::in);
 
-	for (int i = 0; std::getline(fvar, line); i++)
+	for (int i = 0; std::getline(fvar, line); ++i)
 	{
 		switch (i)
 		{
@@ -50,27 +50,36 @@ void Game::LoadPieces(DirectX::SpriteBatch * sprite_batch, ID3D11Device * device
 	std::string line;
 	std::string buffer[14];
 	int index = 0;
+	RECT rect;
 	fvar.open("settings\\types_of_pieces.csv", std::ios::in);
 	std::getline(fvar, line);
 
 	//getting types of pieces
 	while (std::getline(fvar, line))
 	{
-		for (int j = 0; j < line.size(); j++)
+		for (int j = 0; j < static_cast<int>(line.size()); ++j)
 		{
 			if (line[j] == '\t')
-				index++;
+			{
+				++index;
+			}
 			else if (index >= 14)
+			{
 				break;
+			}
 			else
+			{
 				buffer[index] += line[j];
+			}
 		}
 
 		piece_types.insert(std::make_pair(atoi(buffer[0].c_str()), new Piece_type(atoi(buffer[0].c_str()), buffer[2], buffer[3], buffer[5], buffer[4],
 			{ atoi(buffer[6].c_str()),atoi(buffer[7].c_str()),atoi(buffer[8].c_str()),atoi(buffer[9].c_str()) },
 			{ atoi(buffer[10].c_str()),atoi(buffer[11].c_str()),atoi(buffer[12].c_str()),atoi(buffer[13].c_str()) })));
-		for (int j = 0; j < 14; j++)
+		for (int j = 0; j < 14; ++j)
+		{
 			buffer[j] = "";
+		}
 		index = 0;
 	}
 	fvar.close();
@@ -84,14 +93,18 @@ void Game::LoadPieces(DirectX::SpriteBatch * sprite_batch, ID3D11Device * device
 		for (int j = 0; j < line.size(); j++)
 		{
 			if (line[j] == '\t')
-				index++;
+			{
+				++index;
+			}
 			else if (index >= 4)
+			{
 				break;
+			}
 			else
+			{
 				buffer[index] += line[j];
+			}
 		}
-
-		RECT rect;
 		if (atoi(buffer[3].c_str()))
 		{
 			rect = (*piece_types.at(atoi(buffer[2].c_str()))).white;
@@ -124,33 +137,33 @@ void Game::DrawGrid(DirectX::PrimitiveBatch<DirectX::VertexPositionColor> *primi
 	DirectX::XMFLOAT4 colors[2] = { { 0.7529, 0.7529, 0.7529, 1 }, { 1, 0.9215, 0.8039, 1 } };
 	bool current_color = false;
 
-	DirectX::XMFLOAT3 v1(grid_rect.left,  grid_rect.top, 1.0f);
-	DirectX::XMFLOAT3 v2(grid_rect.left + grid_rect.right, grid_rect.top, 1.0f);
-	DirectX::XMFLOAT3 v3(grid_rect.left + grid_rect.right, grid_rect.top + grid_rect.bottom, 1.0f);
-	DirectX::XMFLOAT3 v4(grid_rect.left,  grid_rect.top +  grid_rect.bottom, 1.0f);
+	DirectX::XMFLOAT3 xmfloat3[4] = { DirectX::XMFLOAT3(grid_rect.left,  grid_rect.top, 1.0f),
+		DirectX::XMFLOAT3(grid_rect.left + grid_rect.right, grid_rect.top, 1.0f),
+		DirectX::XMFLOAT3(grid_rect.left + grid_rect.right, grid_rect.top + grid_rect.bottom, 1.0f),
+		DirectX::XMFLOAT3(grid_rect.left,  grid_rect.top + grid_rect.bottom, 1.0f) };
 
 	for (int i = 0; i <cells_in_row; ++i)
 	{
-		v1.x = grid_rect.left;
-		v2.x = grid_rect.left + grid_rect.right;
-		v3.x = grid_rect.left + grid_rect.right;
-		v4.x = grid_rect.left;
+		for (int j = 0; j < 4; ++j)
+		{
+			xmfloat3[j].x = grid_rect.left + grid_rect.right*(j > 0)*(j < 3);
+		}
 		for (int j = 0; j < cells_in_column; ++j)
 		{
 			current_color = !current_color;
-			primitive_batch->DrawQuad(DirectX::VertexPositionColor(XMLoadFloat3(&v1), XMLoadFloat4(&colors[current_color])),
-				DirectX::VertexPositionColor(XMLoadFloat3(&v2), XMLoadFloat4(&colors[current_color])),
-				DirectX::VertexPositionColor(XMLoadFloat3(&v3), XMLoadFloat4(&colors[current_color])),
-				DirectX::VertexPositionColor(XMLoadFloat3(&v4), XMLoadFloat4(&colors[current_color])));
-			v1.x += grid_rect.right;
-			v2.x += grid_rect.right;
-			v3.x += grid_rect.right;
-			v4.x += grid_rect.right;
+			primitive_batch->DrawQuad(DirectX::VertexPositionColor(XMLoadFloat3(&xmfloat3[0]), XMLoadFloat4(&colors[current_color])),
+				DirectX::VertexPositionColor(XMLoadFloat3(&xmfloat3[1]), XMLoadFloat4(&colors[current_color])),
+				DirectX::VertexPositionColor(XMLoadFloat3(&xmfloat3[2]), XMLoadFloat4(&colors[current_color])),
+				DirectX::VertexPositionColor(XMLoadFloat3(&xmfloat3[3]), XMLoadFloat4(&colors[current_color])));
+			for (int k = 0; k < 4; ++k)
+			{
+				xmfloat3[k].x += grid_rect.right;
+			}
 		}
-		v1.y += grid_rect.bottom;
-		v2.y += grid_rect.bottom;
-		v3.y += grid_rect.bottom;
-		v4.y += grid_rect.bottom;
+		for (int j = 0; j < 4; ++j)
+		{
+			xmfloat3[j].y += grid_rect.bottom;
+		}
 		current_color = !current_color;
 	}
 }
@@ -188,7 +201,7 @@ void Game::DrawSpriteBatch(DirectX::SpriteBatch * sprite_batch, float delta_time
 
 void Game::Update(DirectX::Mouse::ButtonStateTracker * button_tracker, DirectX::Mouse * mouse, DirectX::Keyboard::KeyboardStateTracker * keyboard_tracker, DirectX::Keyboard * keyboard, float delta_time)
 {
-	for (int i = 0; i < pieces.size(); i++)
+	for (int i = 0; i < static_cast<int>(pieces.size()); ++i)
 	{
 		if (pieces[i] != nullptr)
 		{
@@ -210,7 +223,7 @@ std::tuple<bool, bool, bool, bool> Game::GetRelativeDirection(int binary_directi
 {
 	bool ret[4];
 
-	int add = ((int)rotation + 45) / 90;
+	int add = (static_cast<int>(rotation) + 45) / 90;
 	add = abs(add);
 
 	ret[(3 + add) % 4] = binary_direction - 8 >= 0;
@@ -229,7 +242,7 @@ void Game::OnMove(int piece_id)
 	std::string move_formula;
 
 	//checking if attack performed
-	for (int i = pieces.size() - 1; i >= 0; i--)
+	for (int i = static_cast<int>(pieces.size()) - 1; i >= 0; --i)
 	{
 		if (pieces[i] != nullptr)
 		{
@@ -277,10 +290,8 @@ void Game::MovePiece(int piece_id)
 
 	pieces[piece_id]->object->SetX(grid_rect.left + x * grid_rect.right);
 	pieces[piece_id]->object->SetY(grid_rect.top + y * grid_rect.bottom);
-
 	pieces[piece_id]->x = pieces[piece_id]->object->GetX();
 	pieces[piece_id]->y = pieces[piece_id]->object->GetY();
-
 	pieces[x + y * cells_in_row].swap(pieces[piece_id]);
 	pieces[piece_id] = nullptr;
 
@@ -295,7 +306,6 @@ bool Game::ValidateMove(std::string move_formula, int piece_id)
 	int src_y = GetCellId_Y(pieces[piece_id]->y);
 	int x_shift = dst_x - src_x;
 	int y_shift = dst_y - src_y;
-
 	bool ret = false;
 
 	if (dst_x == src_x && dst_y == src_y)
@@ -312,33 +322,50 @@ bool Game::ValidateMove(std::string move_formula, int piece_id)
 		int direction = strtoul(((std::string)"" + move_formula[i + 1]).c_str(), NULL, 16);
 		int distance = atoi(((std::string)"" + move_formula[i + 2] + move_formula[i + 3]).c_str());
 		if (distance == 0)
+		{
 			distance = INT_MAX;
+		}
 		bool n, e, w, s;
 		std::tie(n, e, s, w) = GetRelativeDirection(direction, 180 * !pieces[piece_id]->white);
 		if (move_formula[i + 4] == ';')
 		{
 			if ((bool)(x_shift*y_shift) == (bool)str_dgn)
+			{
 				continue;
+			}
 			else if (!str_dgn && abs(x_shift) != abs(y_shift))
+			{
 				continue;
+			}
 			else if (x_shift >= 0 && y_shift > 0 && !n)
+			{
 				continue;
+			}
 			else if (x_shift > 0 && y_shift <= 0 && !e)
+			{
 				continue;
+			}
 			else if (x_shift <= 0 && y_shift < 0 && !s)
+			{
 				continue;
+			}
 			else if (x_shift < 0 && y_shift >= 0 && !w)
+			{
 				continue;
+			}
 			else if (x_shift >= 0 && y_shift > 0 && !n)
+			{
 				continue;
+			}
 			else if (abs(x_shift - y_shift)*str_dgn + abs(x_shift) * !str_dgn > distance)
+			{
 				continue;
+			}
 			else
 			{
 				if (!CollisionDetected(src_x, src_y, dst_x, dst_y))
 					ret = true;
 			}
-
 		}
 	}
 	return ret;
@@ -350,18 +377,19 @@ bool Game::CollisionDetected(int src_x, int src_y, int dst_x, int dst_y)
 	int x_shift = dst_x - src_x;
 	int y_shift = dst_y - src_y;
 	if (abs(x_shift) == abs(y_shift))
+	{
 		str_dgn = false;
-
+	}
 	int distance = abs(x_shift - y_shift)*str_dgn + abs(x_shift) * !str_dgn;
 
-
-
-	for (int i = 0; i < distance - 1; i++)
+	for (int i = 0; i < distance - 1; ++i)
 	{
 		x_shift = x_shift + 1 * (x_shift != 0) - 2 * (x_shift > 0);
 		y_shift = y_shift + 1 * (y_shift != 0) - 2 * (y_shift > 0);
 		if (pieces[src_x + x_shift + (src_y + y_shift)*cells_in_row] != nullptr)
+		{
 			return true;
+		}
 	}
 	return false;
 }
@@ -369,7 +397,7 @@ bool Game::CollisionDetected(int src_x, int src_y, int dst_x, int dst_y)
 
 void Game::ChangePlayer()
 {
-	for (int i = 0; i < pieces.size(); i++)
+	for (int i = 0; i < static_cast<int>(pieces.size()); ++i)
 	{
 		if (pieces[i] != nullptr)
 		{
