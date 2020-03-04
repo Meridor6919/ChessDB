6d6490a996954450f314d62c791d4957af2624b4
Michał Balicki <michalbalicki@o2.pl>
2020-03-04 21:19:38 +0100

    Fixed dumb default msg codes that interfered with basic WM ones

--- ChessDB/Resource.h
+++ ChessDB/Resource.h
@@ -1,27 +1,12 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ChessDB.rc
+#define RC_TOOLBAR 0x401
+#define RC_NEWGAME_PLAYER 0x402
+#define RC_NEWGAME_AI 0x403
+#define RC_SAVEGAME 0x404
+#define RC_GAMES 0x405
+#define RC_PLAYERS 0x406
+#define RC_CHESSBOARD_COLOR 0x407
+#define RC_INTERFACE_COLOR 0x408
+#define RC_FULLSCREEN 0x409
+#define RC_GENERAL 0x40A
+#define RC_PIECES 0x40B
 
-// Next default values for new pieces
-// 
-
-#define RC_TOOLBAR 100
-#define RC_NEWGAME_PLAYER 110
-#define RC_NEWGAME_AI 111
-#define RC_SAVEGAME 112
-#define RC_GAMES 113
-#define RC_PLAYERS 114
-#define RC_CHESSBOARD_COLOR 115
-#define RC_INTERFACE_COLOR 116
-#define RC_FULLSCREEN 117
-#define RC_GENERAL 118
-#define RC_PIECES 119
-
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
